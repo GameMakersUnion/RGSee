46322a70d188d8b45b4333a8ff449a592d4f63b9
Victor <victor.nguyen@hotmail.com>
2015-04-11 18:00:31 -0400

    Updated Damage and Color controller

---
+++ RGSee/Assets/Scripts/Victor/DamageIndicator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageIndicator : MonoBehaviour {
+    public float life = 3f; //The amount of seconds the damage indicator should last
+    private float startTime;
+
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, life); //Blow up after a bit
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
