451209f2abaa5edba2e781cda5d5da318a3e1bb2
Ian Nastajus <ian.nastajus@gmail.com>
2015-04-11 13:20:10 -0400

    moved stuff

--- RGSee/Assets/Scripts/Player.cs
+++ RGSee/Assets/Scripts/Player.cs
@@ -3,9 +3,9 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	public enum Players {Player1, Player2, Player3, Player4}
-
+	public enum Players {Player1, Player2, Player3, Player4 }
 	public Players PlayerNum;
+	public bool halfController = true;
 
 	// Use this for initialization
 	void Start () {
