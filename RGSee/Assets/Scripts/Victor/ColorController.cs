8c1574dba7493edd84018dc08f5a577a1519b4f7
Victor <victor.nguyen@hotmail.com>
2015-04-11 17:37:45 -0400

    Renaming and created color controller, that handles the player colors

---
+++ RGSee/Assets/Scripts/Victor/ColorController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorController : MonoBehaviour {
+    private Manager.Colors[] players;
+
+	// Use this for initialization
+	void Start () {
+        //Setting the player colors
+        players = new Manager.Colors[2];
+        players[0] =  Manager.Colors.Red;
+        players[1] = Manager.Colors.Green;
+        players[2] = Manager.Colors.Blue;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
