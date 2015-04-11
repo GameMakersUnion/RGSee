ad3612c2dc370cd1cf11dff4d2838241408c014d
Victor <victor.nguyen@hotmail.com>
2015-04-11 15:36:44 -0400

    Skeleton Wisp Class created, with prefab

---
+++ RGSee/Assets/Wisp.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wisp : MonoBehaviour {
+    private float radius_ = 1;
+
+    public float radius { get; set; }
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
